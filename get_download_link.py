dfb757ac44400f8dc2609220327281b713a41571
Asım <asimzorlu@gmail.com>
2022-07-05 13:54:56 +0300

    Update get_download_link.py

--- get_download_link.py
+++ get_download_link.py
@@ -14,7 +14,7 @@ driver.get("https://toffeeshare.com/")
 time.sleep(5)
 
 print('Enter your file directory: \n'
-      'for example: C:\\asd\\def\\filename.extension')
+      'For Example: C:\\asd\\def\\filename.extension')
 path = input()
 
 for file in glob.glob(path):
